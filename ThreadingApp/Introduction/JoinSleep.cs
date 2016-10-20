8d99e37ef7b56624469464953198f7edded8d987
Jeffry Angtoni <jeffryangtoni24@gmail.com>
2016-10-20 19:21:44 +0700

    #20161020 -> ThreadStart

--- ThreadingApp/Introduction/JoinSleep.cs
+++ ThreadingApp/Introduction/JoinSleep.cs
@@ -1,29 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading;
+//using System.Threading.Tasks;
 
-namespace Introduction
-{
-    class JoinSleep
-    {
-        static void Main(String[] args)
-        {
-            Thread tt = new Thread(Go);
-            Thread.Sleep(2000);
-            tt.Start();
-            tt.Join();
-            Console.WriteLine("Thread has Ended!");
-        }
+//namespace Introduction
+//{
+//    class JoinSleep
+//    {
+//        static void Main(String[] args)
+//        {
+//            Thread tt = new Thread(Go);
+//            Thread.Sleep(2000);
+//            tt.Start();
+//            tt.Join();
+//            Console.WriteLine("Thread has Ended!");
+//        }
 
-        static void Go()
-        {
-            for(int i = 0; i < 1000; i++)
-            {
-                Console.Write("y");
-            }
-        }
-    }
-}
+//        static void Go()
+//        {
+//            for(int i = 0; i < 1000; i++)
+//            {
+//                Console.Write("y");
+//            }
+//        }
+//    }
+//}
---
+++ ThreadingApp/Introduction/ThreadStartApp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Introduction
+{
+    class ThreadStartApp
+    {
+        static void Main(String[] args)
+        {
+            //Thread t = new Thread(new ThreadStart(Go));
+            Thread t = new Thread(() => Console.WriteLine("Hello!"));   // use lambda
+            t.Start();
+
+            //Go();
+        }
+
+        static void Go()
+        {
+            Console.WriteLine("Hello!");
+        }
+    }
+}
