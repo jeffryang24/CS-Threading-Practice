a63454c81516e4faebfaf1ab70fad7d161aa4dde
Jeffry Angtoni <jeffryangtoni24@gmail.com>
2016-10-21 17:51:25 +0700

    #20161021 -> Comment

--- ThreadingApp/Introduction/ThreadStartApp.cs
+++ ThreadingApp/Introduction/ThreadStartApp.cs
@@ -1,26 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading;
+//using System.Threading.Tasks;
 
-namespace Introduction
-{
-    class ThreadStartApp
-    {
-        static void Main(String[] args)
-        {
-            //Thread t = new Thread(new ThreadStart(Go));
-            Thread t = new Thread(() => Console.WriteLine("Hello!"));   // use lambda
-            t.Start();
+//namespace Introduction
+//{
+//    class ThreadStartApp
+//    {
+//        static void Main(String[] args)
+//        {
+//            //Thread t = new Thread(new ThreadStart(Go));
+//            Thread t = new Thread(() => Console.WriteLine("Hello!"));   // use lambda
+//            t.Start();
 
-            //Go();
-        }
+//            //Go();
+//        }
 
-        static void Go()
-        {
-            Console.WriteLine("Hello!");
-        }
-    }
-}
+//        static void Go()
+//        {
+//            Console.WriteLine("Hello!");
+//        }
+//    }
+//}
