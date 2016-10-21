10f1a93ec8b3a7bdb9da3e27b9065f5e6165ba9d
Jeffry Angtoni <jeffryangtoni24@gmail.com>
2016-10-21 17:51:13 +0700

    #20161021 -> Passing param to thread

---
+++ ThreadingApp/Introduction/PassParamThread.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Introduction
+{
+    class PassParamThread
+    {
+        static void Main()
+        {
+            //Thread t = new Thread(() =>
+            //{
+            //    printMsg("Salam Lemper!");
+            //});
+            Thread t = new Thread(() => printMsg("Jancuk!!"));
+            t.Start();
+        }
+
+        static void printMsg(String a)
+        {
+            Console.WriteLine("Message : " + a);
+        }
+    }
+}
