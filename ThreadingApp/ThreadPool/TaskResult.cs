efd335c10cb84bc97f3472bf5fd1907b226a873c
Jeffry Angtoni <jeffryangtoni24@gmail.com>
2016-10-30 13:19:25 +0700

    #20161030 -> Daytime [Tasks]

---
+++ ThreadingApp/ThreadPool/TaskResult.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ThreadPool
+{
+    class TaskResult
+    {
+        static void Main()
+        {
+            Task<String> task = Task.Factory.StartNew(() => DownloadString("http://www.google.com"));
+            Console.Write("Please wait...");
+
+            string result = task.Result;
+            Console.WriteLine(result);
+        }
+
+        static string DownloadString(string url)
+        {
+            using(var wc = new System.Net.WebClient())
+            {
+                return wc.DownloadString(url);
+            }
+        }
+    }
+}
