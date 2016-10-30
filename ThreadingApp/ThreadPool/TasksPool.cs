1604efdead3846ceddd6c821b9336ec3c2061c33
Jeffry Angtoni <jeffryangtoni24@gmail.com>
2016-10-30 11:11:02 +0700

    #20161030 -> Introduction to Thread Pool

---
+++ ThreadingApp/ThreadPool/TasksPool.cs
@@ -0,0 +1,22 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.Threading;
+
+//namespace ThreadPool
+//{
+//    class TasksPool
+//    {
+//        static void Main(String[] args)
+//        {
+//            Task.Factory.StartNew(Go);
+//        }
+
+//        static void Go()
+//        {
+//            Console.WriteLine("Hello from the thread pool!");
+//        }
+//    }
+//}
